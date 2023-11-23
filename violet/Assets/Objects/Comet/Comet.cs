3d1bcb5e10c3ad7b5172ab14979b753cc618f61d
CR-Daniel <choudaniel3@gmail.com>
2023-11-24 00:15:08 +0100

    comet haptic feedback

--- violet/Assets/Objects/Comet/Comet.cs
+++ violet/Assets/Objects/Comet/Comet.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class Comet : MonoBehaviour
 {
@@ -16,10 +17,19 @@ public class Comet : MonoBehaviour
         if (other.CompareTag("String") && !isHitByRacket)
         {
             isHitByRacket = true;
-            // enable fire vfx?
-            // haptic feedback (event)
-            // sfx
+
             AdjustCometVelocity(other);
+
+            // sfx
+
+            // Calculate haptic feedback intensity based on hit speed
+            Rigidbody cometRigidbody = GetComponent<Rigidbody>();
+            Debug.Log(cometRigidbody.velocity.magnitude);
+            float hitIntensity = Mathf.Clamp(cometRigidbody.velocity.magnitude / 40f, 0f, 1f);
+
+            // Send haptic feedback to the right hand controller
+            SendHapticFeedback(XRNode.RightHand, hitIntensity, 0.3f);
+
             Destroy(gameObject, lifespan);
             onCometHitRacket.Raise();
         }
@@ -33,7 +43,10 @@ public class Comet : MonoBehaviour
             Vector3 impactPosition = collision.contacts[0].point;
             SpawnVFX(impactPosition);
 
-            // haptic feedback (event)
+            // Send a slight rumble to both controllers
+            SendHapticFeedback(XRNode.RightHand, 0.3f, 0.3f);
+            SendHapticFeedback(XRNode.LeftHand, 0.3f, 0.3f);
+
             // sfx
             onCometHitEnemy.Raise();
             Destroy(gameObject);
@@ -57,7 +70,16 @@ public class Comet : MonoBehaviour
             Vector3 averageVelocity = racketFollower.GetAverageVelocity();
 
             Rigidbody cometRigidbody = GetComponent<Rigidbody>();
-            cometRigidbody.velocity = averageVelocity * 2.0f;
+            cometRigidbody.velocity = averageVelocity * 2.0f; 
+        }
+    }
+
+    private void SendHapticFeedback(XRNode controllerNode, float intensity, float duration)
+    {
+        var hapticDevice = InputDevices.GetDeviceAtXRNode(controllerNode);
+        if (hapticDevice.isValid)
+        {
+            hapticDevice.SendHapticImpulse(0, intensity, duration);
         }
     }
 }
